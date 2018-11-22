79a4df60b824682d0e2c3d7ee086bb8f1c38f8ab
fhyy <fhyy@kth.se>
2018-11-22 18:30:26 +0100

    some cleaning

--- src/rosie_rrt_service.cpp
+++ src/rosie_rrt_service.cpp
@@ -17,7 +17,7 @@ bool RequestRerun(rosie_map_controller::RequestRerun::Request &req,rosie_map_con
 	}
 	ROS_INFO("%d", rerun_command);
 	return true;
-	
+
 }
 
 bool CommissionRerun(rosie_map_controller::CommissionRerun::Request &req, rosie_map_controller::CommissionRerun::Response &res){
@@ -37,6 +37,17 @@ bool CommissionRerun(rosie_map_controller::CommissionRerun::Request &req, rosie_
 	return true;
 }
 
+bool start_command = 0;
+bool StartPathPlanning(rosie_map_controller::StartRRT::Request &req, rosie_map_controller::StartRRT::Response &res){
+	if(req.command == 1){
+		start_command = 1;
+		res.answer = 1;
+	}else if(req.command == 2){
+		res.answer = start_command;
+	}
+
+	return true;
+}
 
 int main(int argc, char **argv)
 {
@@ -47,6 +58,7 @@ int main(int argc, char **argv)
   //ros::ServiceClient client = n.serviceClient<arduino_servo_control::SetServoAngles>("SetServoAngles");
   ros::ServiceServer ReqService = n.advertiseService<rosie_map_controller::RequestRerun::Request, rosie_map_controller::RequestRerun::Response>("request_rerun", RequestRerun);
 	ros::ServiceServer CommService = n.advertiseService<rosie_map_controller::CommissionRerun::Request, rosie_map_controller::CommissionRerun::Response>("commission_rerun", CommissionRerun);
+	ros::ServiceServer StartService = n.advertiseService<rosie_map_controller::StartRRT::Request, rosie_map_controller::StartRRT::Response>("start_rrt", StartPathPlanning);
   //ros::Subscriber sub = n.subscribe("MoveGates", 1000, GateCallback);
   ROS_INFO("Ready to move the grippers.");
   ros::spin();
